d7da37832ffe6534b9c33d86b31f5220c3c9ccfd
wangjinbo <jinbo@mamahao.com>
2017-05-18 16:20:42 +0800

    提交注册中心Eureka Server、Client模块

---
+++ cloud-eureka-client/src/main/java/com/king/eureka/client/ServiceHelloApplication.java
@@ -0,0 +1,32 @@
+package com.king.eureka.client;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author wangjinbo
+ * @create 2017-05-18上午 11:06
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+public class ServiceHelloApplication {
+
+    @Value("${server.port}")
+    private String port;
+
+    @RequestMapping("/hi")
+    public String sayHi(@RequestParam String name) {
+        return "hi " + name + ", i am from port : " + port;
+
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceHelloApplication.class, args);
+    }
+}
