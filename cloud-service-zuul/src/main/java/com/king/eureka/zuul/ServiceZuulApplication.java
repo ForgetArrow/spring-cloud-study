bf12ead2672e5141e6faefdedd562f8b02c4a72d
wangjinbo <jinbo@mamahao.com>
2017-07-20 16:38:04 +0800

    1）提交zuul路由服务；2）升级cloud版本

--- cloud-service-zuul/src/main/java/com/king/eureka/zuul/ServiceZuulApplication.java
+++ cloud-service-zuul/src/main/java/com/king/eureka/zuul/ServiceZuulApplication.java
@@ -1,10 +1,22 @@
 package com.king.eureka.zuul;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
 /**
  * cloud-zuul路由网关服务
+ * 路由、服务过滤、安全校验等等
  *
  * @author wangjinbo
  * @create 2017-07-20下午 3:29
  */
+@SpringBootApplication
+@EnableZuulProxy
+@EnableEurekaClient
 public class ServiceZuulApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceZuulApplication.class, args);
+    }
 }
