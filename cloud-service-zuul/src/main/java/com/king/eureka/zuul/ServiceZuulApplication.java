b4ce66e0d155fca0fc7a9019dbe4a61bd530c340
wangjinbo <jinbo@mamahao.com>
2017-07-20 16:35:39 +0800

    1）zuul路由网关服务提交；2）升级cloud版本

---
+++ cloud-service-zuul/src/main/java/com/king/eureka/zuul/ServiceZuulApplication.java
@@ -0,0 +1,10 @@
+package com.king.eureka.zuul;
+
+/**
+ * cloud-zuul路由网关服务
+ *
+ * @author wangjinbo
+ * @create 2017-07-20下午 3:29
+ */
+public class ServiceZuulApplication {
+}
---
+++ cloud-service-zuul/src/main/java/com/king/eureka/zuul/filter/MyFilter.java
@@ -0,0 +1,8 @@
+package com.king.eureka.zuul.filter;
+
+/**
+ * @author wangjinbo
+ * @create 2017-07-20下午 4:04
+ */
+public class MyFilter {
+}
