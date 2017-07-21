22022ba8980372e3b71abf769613c8b8f059f720
wangjinbo <jinbo@mamahao.com>
2017-07-21 16:51:43 +0800

    配置中心更改为eureka服务实例

--- cloud-config-server/src/main/java/com/king/cloud/config/server/ApplicationConfigServer.java
+++ cloud-config-server/src/main/java/com/king/cloud/config/server/ApplicationConfigServer.java
@@ -2,6 +2,7 @@ package com.king.cloud.config.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
@@ -12,6 +13,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
  */
 @SpringBootApplication
 @EnableConfigServer
+@EnableDiscoveryClient
 public class ApplicationConfigServer {
     public static void main(String[] args) {
         SpringApplication.run(ApplicationConfigServer.class, args);
